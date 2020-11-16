38efacb68b405f55ce276c18c89da4a9a9f5cc61
kenmono <ratchapon_pukkham@cmu.ac.th>
2020-11-16 10:22:14 +0700

    create file Lab05_1_620510660.py

---
+++ Lab05_1_620510660.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# รัฏชพล ปุกคำ
+# 620510660
+# Lab 05
+# Problem 1
+# 204113 Sec 002
+import time
+import datetime
+from pylab import *
+def main():
+    n = int(input())
+    rand_float(n)
+def rand_float(n):
+    x = (int)(time.time()) 
+    l = []
+    for i in range(n):
+        y = datetime.datetime.now()
+        a = (int)(y.microsecond) 
+        m = (int)(y.minute*y.second)
+        c = (int)(y.minute*y.second)
+        xn = (a*x+c) % m
+        xn = xn/m
+        l.append(xn) 
+        x = xn #เปลี่ยนseedไปเรื่อยๆ
+    plot(l,'ro')
+    show()
+    
+if __name__ == '__main__' :
+    main()
+    
